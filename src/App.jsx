cbeb5f14564190bc184d828505fdcd6d43902685
Hayyat Hussain <hayyathussain73@gmail.com>
2024-03-26 23:24:22 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-zinc-900 text-white">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
