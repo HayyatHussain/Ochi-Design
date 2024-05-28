68bdb3ee5e8a63d05dff13c6a5876d64ac3f52de
Hayyat Hussain <hayyathussain73@gmail.com>
2024-05-28 11:52:28 +0500

    Styling Updated

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,13 +3,13 @@ import { PiArrowUpRightBold } from "react-icons/pi";
 
 function landing() {
   return (
-    <div className="w-full h-screen bg-zinc-900 pt-1">
+    <div className="w-full min-h-screen bg-zinc-900 pt-1">
       <div className="textStructure mt-36 px-[3.35rem]">
         {["we create", "eye opening", "presentations"].map((item, index) => (
           <div className="masker">
             <div className="w-fit flex gap-3 overflow-hidden">
               {index === 1 && (
-                <div className="w-[8rem] h-[5.4rem] bg-red-500 self-end rounded-[8px]"></div>
+                <div className="w-[8rem] h-[5.4rem] self-end rounded-[8px] ani-pic"></div>
               )}
               <h1 key={index} className="text-9xl uppercase leading-[100px]">
                 {item}
@@ -18,7 +18,7 @@ function landing() {
           </div>
         ))}
       </div>
-      <div className="font-['Neue_Montreal'] font-regular text-md flex justify-between items-center border-t-[1px] mt-60 border-zinc-600 px-[3.35rem] py-3">
+      <div className="font-['Neue_Montreal'] font-regular text-md flex justify-between items-center border-t-[1px] mt-64 border-zinc-600 px-[3.35rem] py-3">
         {[
           "For public and private companies",
           "From the first pitch to IPO",
