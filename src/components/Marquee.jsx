9b0b5b3de99e2ce7c21736c69ef7bbe0bfe90b7e
Hayyat Hussain <hayyathussain73@gmail.com>
2024-06-17 21:15:42 +0500

    Animations added

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,9 +4,24 @@ import React from "react";
 function Marquee() {
   return (
     <section className="w-full bg-[#004D43] py-[6.2rem] rounded-t-[1.2rem]">
-      <div className="text text-[21.5rem] font-semibold founder flex items-center gap-10 border-y-2 border-zinc-300 leading-[0.82] whitespace-nowrap overflow-hidden">
-        <h1 className="-mt-[2.27rem]">WE ARE OCHI</h1>
-        <h1 className="-mt-[2.27rem]">WE ARE OCHI</h1>
+      <div className="text text-[21.5rem] font-semibold founder flex items-center border-y-2 border-zinc-300 leading-[0.82] whitespace-nowrap overflow-hidden">
+        <motion.div
+          initial={{ x: 0 }}
+          animate={{ x: "-50%" }}
+          transition={{ repeat: Infinity, ease: "linear", duration: 34 }}
+          className="flex"
+        >
+          {[
+            "WE ARE OCHI",
+            "WE ARE OCHI",
+            "WE ARE OCHI",
+            "WE ARE OCHI",
+            "WE ARE OCHI",
+            "WE ARE OCHI",
+          ].map((item, index) => (
+            <h1 className={`-mt-[2.27rem] ${index != 5 && "pr-10"}`}>{item}</h1>
+          ))}
+        </motion.div>
       </div>
     </section>
   );
