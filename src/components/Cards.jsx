75e3d1d573372b3ec097eeb120497a46d1ac1f52
Hayyat Hussain <hayyathussain73@gmail.com>
2024-06-17 16:47:52 +0500

    3 Cards added

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,16 +6,40 @@ function Cards() {
       <div className="card-con w-1/2 h-[20.5rem]">
         <div className="relative flex items-center justify-center card w-full h-full bg-[#004D43] rounded-lg">
           <img
+            className="w-[8.6rem]"
             src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg"
             alt="svg"
           />
-          <button className="absolute left-10 bottom-8 neue">©2019-2022</button>
+          <button
+            disabled="true"
+            className="absolute left-7 bottom-7 neue text-[#cdea68] text-[13px] border-[1px] border-[#cdea68] rounded-2xl py-[0.1rem] px-2"
+          >
+            ©2019-2022
+          </button>
         </div>
       </div>
       <div className="card-con w-1/2 h-[20.5rem]">
         <div className="cards w-full h-full rounded-lg flex gap-3">
-          <div className="cards w-1/2 h-full bg-[#212121] rounded-lg"></div>
-          <div className="cards w-1/2 h-full bg-[#212121] rounded-lg"></div>
+          <div className="card relative w-1/2 h-full bg-[#212121] rounded-lg flex items-center justify-center">
+            <img
+              className="w-[8.6rem]"
+              src="https://ochi.design/wp-content/uploads/2022/04/logo002.svg"
+              alt="svg"
+            />
+            <button className="absolute left-7 bottom-7 neue text-[#e7e7e7] text-[13px] border-[1px] border-[#e7e7e7] rounded-2xl py-[0.1rem] px-2">
+              RATING 5.0 ON CLUTCH
+            </button>
+          </div>
+          <div className="card relative w-1/2 h-full bg-[#212121] rounded-lg flex items-center justify-center">
+            <img
+              className="w-[6.05rem]"
+              src="https://ochi.design/wp-content/uploads/2022/04/logo003.png"
+              alt="svg"
+            />
+            <button className="absolute left-7 bottom-7 neue text-[#e7e7e7] text-[13px] border-[1px] border-[#e7e7e7] rounded-3xl py-[0.28rem] px-8 leading-[14px]">
+              BUSINESS BOOTCAMP <br></br> ALUMNI
+            </button>
+          </div>
         </div>
       </div>
     </div>
